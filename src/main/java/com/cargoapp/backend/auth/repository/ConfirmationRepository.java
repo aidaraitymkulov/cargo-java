16020ff8250312a3bcbbaf1cd8820ef6032897c4
aidaraitymkulov <aitymkulovich@gmail.com>
2026-02-23 18:05:49 +0600

    created repositories for auth flow

---
+++ src/main/java/com/cargoapp/backend/auth/repository/ConfirmationRepository.java
@@ -0,0 +1,12 @@
+package com.cargoapp.backend.auth.repository;
+
+import com.cargoapp.backend.auth.entity.ConfirmationEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ConfirmationRepository extends JpaRepository<ConfirmationEntity, UUID> {
+    Optional<ConfirmationEntity> findByUser_Id(UUID userId);
+    void deleteByUser_Id(UUID userId);
+}
